cbea2e735c8b6d3af97988145936539b3db83a36
DLR <david.lopez-rodriguez@tecalliance.net>
2021-10-26 21:15:43 +0100

    cors disabled

--- VisitorsTracker.API/Startup.cs
+++ VisitorsTracker.API/Startup.cs
@@ -54,6 +54,13 @@ namespace VisitorsTracker.API
 
             app.UseRouting();
 
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
